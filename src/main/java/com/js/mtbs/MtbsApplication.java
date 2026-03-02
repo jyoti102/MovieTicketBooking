c9d53acc0eb7eb6bad6bcf2ca33159623e6eea74
Jyoti Singh <jyoti.singhd102@gmail.com>
2026-03-02 20:43:22 +0530

    Initial commit

---
+++ src/main/java/com/js/mtbs/MtbsApplication.java
@@ -0,0 +1,13 @@
+package com.js.mtbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtbsApplication.class, args);
+	}
+
+}
